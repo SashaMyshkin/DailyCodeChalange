c45b66e30b332fd724c273f46f6858c5ec47e9ae
Sasha Myshkin <homocalidus@gmail.com>
2025-09-17 22:19:26 +0200

    generateSlug

---
+++ 09/2025-09-17-generateSlug.js
@@ -0,0 +1,16 @@
+/*
+ * The task was very simple:
+ * Given a string, return a URL-friendly version of the string using the following constraints:
+ * All letters should be lowercase.
+ * All characters that are not letters, numbers, or spaces should be removed.
+ * All spaces should be replaced with the URL-encoded space code %20.
+ * Consecutive spaces should be replaced with a single %20.
+ * The returned string should not have leading or trailing %20.
+ */
+function generateSlug(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9 ]/g, "")
+    .replace(/[ ]+/g, "%20");
+}
